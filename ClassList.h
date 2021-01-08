aead57bb17d08ce4758d361974c578e34d5c7a2f
Tomer Bitan <tbitan@gmail.com>
2021-01-08 17:21:14 +0200

    added class list and constructor with defaults

---
+++ ClassList.h
@@ -0,0 +1,28 @@
+#ifndef CLASSLIST
+#define CLASSLIST
+
+#include "Array.h"
+#include <memory>
+
+class ClassList
+{
+    private:
+    std::shared_ptr<Array<int>> classes;
+    int numOfClasses;
+
+    void GrowArray();
+
+    public:
+    ClassList();
+    ClassList& operator=(const ClassList& other) = delete;
+    ~ClassList() = default;
+    void AddClass();
+    void WatchClass(int classID, int time);
+    int TimeWatched(int classID);
+
+
+
+
+};
+
+#endif //ClassList
---
+++ HashTable.h
@@ -0,0 +1,19 @@
+#ifndef HASHTABLE
+#define HASHTABLE
+
+#include "Array.h"
+#include <memory>
+
+class HashTable
+{
+    private:
+    public:
+    HashTable(/* args */);
+    ~HashTable();
+
+
+};
+
+
+
+#endif//HASH TABLE
